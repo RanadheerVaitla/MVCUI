87e1bfc2a5dd55066d2ef4e4e830c81888d2be57
vytlaranadheer02 <vytlaranadheer02@gmail.com>
2022-12-17 13:25:41 +0530

    Added new controller

---
+++ MVCUIDesigning/Controllers/ButtonsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class ButtonsController : Controller
+    {
+        // GET: Buttons
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
